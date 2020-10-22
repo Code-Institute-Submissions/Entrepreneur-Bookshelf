8604d0e22269c365a21baaaf5ce3516af276e3ea
Developer Hope <me@devhope.co>
2020-10-22 10:10:29 +0000

    Added input fields to the add a book form and customised them

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
   $(".sidenav").sidenav({ edge: "right" });
   $(".collapsible").collapsible();
   $(".tooltipped").tooltip();
+  $(".datepicker").datepicker({
+    format: "dd mmmm, yyyy",
+    yearRange: [2020, 2023],
+    showClearBtn: true,
+    i18n: {
+      done: "Select",
+    },
+  });
 });
