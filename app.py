49fe538866cb4694c50280a29f6c6baffdc977ec
Developer Hope <me@devhope.co>
2020-10-26 11:54:53 +0000

    Added my books to reader's profile and improved collapsible for mobile

--- app.py
+++ app.py
@@ -82,7 +82,8 @@ def profile(username):
         {"username": session["reader"]})["username"]
 
     if session["reader"]:
-        return render_template("profile.html", username=username)
+        books = list(mongo.db.books.find())
+        return render_template("profile.html", username=username, books=books)
 
     return redirect(url_for("login"))
 
